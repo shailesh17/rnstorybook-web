4ec79add1c5ba13aa0167c4ba5a9fdd6932979c4
Shailesh Patel <shaileshpatel@rccl.com>
2021-09-07 12:08:04 -0400

    add react-native-web, and npm run start-react

---
+++ App.web.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View, Text, ActivityIndicator} from 'react-native';
+
+/**
+ * This is the index file for the storybook-web index file.
+ */
+function App() {
+  return (
+    <View>
+        <Text>Hello world from react native</Text>
+        <ActivityIndicator />
+    </View>
+  )
+}
+
+export default App
